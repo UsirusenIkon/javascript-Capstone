fb68e67223003bc083898bf4b85c3af200405e5d
Okechukwu <okei.muokwugwo@outlook.com>
2022-10-18 15:41:48 +0100

    Add modules directory and getMeal.js

--- src/modules/getMeal.js
+++ src/modules/getMeal.js
@@ -1,12 +1,16 @@
-const getMeal = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '9b12e19dcfmsh710e1ac0dde1a54p13a36bjsnd251b54c2243',
-    'X-RapidAPI-Host': 'tasty.p.rapidapi.com',
-  },
+const getMenuList = async () => {
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '9b12e19dcfmsh710e1ac0dde1a54p13a36bjsnd251b54c2243',
+      'X-RapidAPI-Host': 'tasty.p.rapidapi.com',
+
+    },
+  };
+  await fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes', options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
 };
 
-fetch('https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=chicken%20soup', getMeal)
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((err) => console.error(err));
+getMenuList();
