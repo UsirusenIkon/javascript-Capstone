e6a1c86a2f7c88a98beee03443cd157281425f60
Usirusen Ikon <lexicon048@gmail.com>
2022-10-19 16:04:17 +0100

    Remove loadmodal function

--- src/index.js
+++ src/index.js
@@ -1,37 +1,11 @@
 import './style.css';
+import getMenuList from './modules/getMeal.js';
+import renderData from './modules/renderData.js';
 
-const commentBtn = document.querySelector('.comment');
-const modalFilter = document.querySelector('.modal-filter');
-const modalcontent = document.querySelector('.modal');
-
-const loadModal = () => {
-  modalcontent.innerHTML = `<i class="bx bx-x"></i>
-  <img src="" alt="">
-  <span class="name">name:</span><span class="description">description</span>
-  <div class="comments"></div>
-  <div class="comment-form">
-    <form class="modal-form flex" >
-      <label for="Name">
-        <input class="leader user" name="Name" type="text" placeholder="your name" maxlength="30" required>
-      </label>
-      <label for="Textarea">
-        <input class="leader user" name="textarea" type="text" placeholder="your comment" maxlength="80" required>
-      </label>
-      <button class="submit" type="submit">Comment</i></button>
-    </form>
-  </div>`;
+const loadFoodData = () => {
+  getMenuList()
+    .then((response) => response.json())
+    .then((data) => renderData(data.results));
 };
 
-loadModal();
-
-commentBtn.addEventListener('click', () => {
-  modalFilter.classList.toggle('open');
-  modalcontent.classList.toggle('open');
-
-  const closeBtn = document.querySelector('.bx-x');
-  closeBtn.addEventListener('click', () => {
-    modalFilter.classList.remove('open');
-    modalcontent.classList.remove('open');
-  });
-});
-
+window.onload = loadFoodData();
