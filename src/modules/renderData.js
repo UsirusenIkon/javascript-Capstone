68d81c4fe52684b9817496cd1286aae0e7d83b5f
Okechukwu <okei.muokwugwo@outlook.com>
2022-10-20 12:56:43 +0100

    Update the files

--- src/modules/renderData.js
+++ src/modules/renderData.js
@@ -1,3 +1,8 @@
+import postComment from './postComments.js';
+import getUserComment, { countUserComment } from './getComments.js';
+import postUserLike from './postLikes.js';
+import getUserLike from './getLikes.js';
+
 const renderData = (arr) => {
   const foodList = document.querySelector('.foods');
   arr.forEach((card) => {
@@ -6,7 +11,7 @@ const renderData = (arr) => {
       <img class="br" src=${card.thumbnail_url} alt=${card.thumbnail_alt_text}>
     </div>
     <div class="like flex">
-      <span class="name">Name:<i class="desc">${card.name}</i></span><i class='bx bxs-heart'></i>
+      <span class="name">Name:<i class="desc">${card.name}</i></span><i  class='bx bxs-heart'></i>
     </div>
     <button class="comment">Comments</button>
     </div>`;
@@ -16,6 +21,10 @@ const renderData = (arr) => {
     commentBtn.forEach((btn, id) => btn.addEventListener('click', () => {
       const modalcontent = document.querySelector('.modal');
 
+      // Get and Count comments
+      const userComment = getUserComment(id);
+      const commentCount = countUserComment(id);
+
       modalcontent.innerHTML = `<i class="bx bx-x"></i>
       <div class="content flex">    
         <img class="br" src=${arr[id].thumbnail_url} alt="">
@@ -24,18 +33,33 @@ const renderData = (arr) => {
           <span class="description">summary:<i class="desc">${arr[id].description}</i></span>
         </div>
       </div>
-      <div class="comments"></div>
+      <span>Comments(${commentCount})</span>
+      <div class="comments">${userComment.length >= 1
+    ? userComment.map((user) => `<p>${user.creation_date} ${user.username} : ${user.comment}</p>`).join(' ') : '<p> "make a comment" </p>'}</div>
       <div class="comment-form">
         <form class="modal-form flex">
           <label for="Name">
             <input class="leader user" name="Name" type="text" placeholder="your name" maxlength="30" required>
           </label>
           <label for="Textarea">
-            <input class="leader comment" name="textarea" type="text" placeholder="your comment" maxlength="80" required>
+            <input class="leader comment" id="comment"name="textarea" type="text" placeholder="your comment" maxlength="80" required>
           </label>
           <button class="submit" type="submit">Comment</i></button>
         </form>
       </div>`;
+      const addComment = document.querySelector('.modal-form');
+      addComment.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const name = document.querySelector('.user').value;
+        const comment = document.querySelector('#comment').value;
+        postComment(id, name, comment);
+        getUserComment(id);
+        document.querySelector('.modal-form').reset();
+      });
+      document.querySelector('.bxs-heart').addEventListener('click', async () => {
+        await postUserLike();
+        await getUserLike();
+      });
       const body = document.querySelector('body');
       const modalFilter = document.querySelector('.modal-filter');
       modalFilter.classList.toggle('open');
