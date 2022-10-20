e72b400aefb337c32ce43b11eeeceb8d4ba85a81
Okechukwu <okei.muokwugwo@outlook.com>
2022-10-20 12:56:21 +0100

    Add postComments.js file

---
+++ src/modules/postComments.js
@@ -0,0 +1,21 @@
+const postComment = async (id, name, comment) => {
+  const data = {
+    item_id: id,
+    username: name,
+    comment,
+  };
+  console.log(data);
+  const API_URL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/BN8MvkOs74Ofyu4CfHNx/comments';
+  const response = await fetch(API_URL, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const data2 = await response.json();
+  console.log(data2);
+};
+
+export default postComment;
