2a34d38be310f4fd3f20b23b4deaab7958660238
Usirusen Ikon <lexicon048@gmail.com>
2022-10-21 13:56:15 +0100

    Implement getcomment function

--- src/modules/getComments.js
+++ src/modules/getComments.js
@@ -1,14 +1,7 @@
 const getUserComment = async (id) => {
   const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/BN8MvkOs74Ofyu4CfHNx/comments?item_id=${id}`);
-  const data = response.json();
-  console.log(data);
+  const data = await response.json();
+  return data;
 };
 
 export default getUserComment;
-
-// comment counter
-export const countUserComment = async (id) => {
-  const userComment = await getUserComment(id);
-  console.log(userComment.length);
-  return userComment.length;
-};
